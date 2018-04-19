86edc5c51f83328585d28bbe193fc24aa363fbde
alexthayn <00148946@badgermail.snow.edu>
2018-04-19 17:44:37 +0000

    first commit

---
+++ src/main/java/alex/thayn/SodaSelect.java
@@ -0,0 +1,34 @@
+package alex.thayn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SodaSelect{
+
+	public List getAvailableBrands(SodaType type){
+		List brands = new ArrayList();
+
+	
+		if(type.equals(SodaType.PEPSICO)){
+			brands.add("Pepsi");
+			brands.add("Mug Root Beer");
+			brands.add("Mountain Dew");
+		}
+		else if(type.equals(SodaType.COCACOLA)){
+			brands.add("Coca-Cola");
+			brands.add("Sprite");
+			brands.add("Fanta");
+			brands.add("Fresca");
+		}	
+		else if(type.equals(SodaType.DRPEPPERSNAPPLE)){
+			brands.add("7up");
+			brands.add("A&W Root Beer");
+			brands.add("Crush");
+		}
+		else{
+			brands.add("No Brand Available");
+		}
+		
+		return brands;
+	}
+}
---
+++ src/main/java/alex/thayn/SodaType.java
@@ -0,0 +1,6 @@
+package alex.thayn;
+
+public enum SodaType{
+	PEPSICO, COCACOLA, DRPEPPERSNAPPLE
+}
+
