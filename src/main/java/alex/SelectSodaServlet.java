df367151a80d39479acc99a7fec2b7675edfe292
alexthayn <alexthayn>
2018-04-23 21:45:22 -0600

    Fixed dynamic generation of sodas

--- src/main/java/alex/SelectSodaServlet.java
+++ src/main/java/alex/SelectSodaServlet.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(
@@ -18,10 +23,43 @@ import java.util.List;
 )
 
 public class SelectSodaServlet extends HttpServlet{
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://jacksonmysqldb.ck9qmzz9yyn6.us-west-2.rds.amazonaws.com";
+	static final String USER = "alex";
+	static final String PASS = "1234";
+	
+	java.sql.Connection conn = null;
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 	String sodaType = req.getParameter("Type");
+	
+	List<String> sodas = new ArrayList<String>();
 
+	//Connect to my db
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Connecting to a soda database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connected to database successfully...");
+			
+			String sqlquery = "SELECT * FROM alexdb.Sodas Inner Join alexdb.SodaCompanies on Sodas.SodaCompanyId = SodaCompanies.Id Where SodaCompanies.Id = " + sodaType;
+			PreparedStatement sqlstatement = conn.prepareStatement(sqlquery);
+			ResultSet result = sqlstatement.executeQuery();
+			
+			while(result.next()) {
+				String name =  result.getString(1);
+				sodas.add(name);
+				System.out.println(name);
+			}
+				
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			// Handle errors class exceptions
+		} catch (SQLException e) {
+			// Handle errors for JDBC
+		}
+	
 	SodaSelect sodaSelect = new SodaSelect();
 	SodaType l = SodaType.valueOf(sodaType);
 	
